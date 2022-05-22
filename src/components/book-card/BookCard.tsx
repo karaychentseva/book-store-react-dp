e5315e49edd61c7d6c1a8b9377ed86e35fd84a40
Natalya <natalya.karaychentseva@gmail.com>
2022-05-22 12:01:27 +0300

    BookCard - add props

--- src/components/book-card/BookCard.tsx
+++ src/components/book-card/BookCard.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import "./BookCard.scss";
 
 type PropsType = {
+    img: string,
+    title: string,
+    subtitle: string,
+    price: string
 }
 
-const BookCard: React.FC<PropsType> = () => {
+const BookCard: React.FC<PropsType> = ({img, title, subtitle, price}) => {
 
     return (
         <div className="book-card">
             <div className="book-card__img">
-                <img src="https://itbook.store/img/books/9781491985571.png" className='book-card__img-item' />
+                <img src={img} className='book-card__img-item' />
             </div>
-            <h4 className="book-card__title">Web Scraping with Python, 2nd Edition</h4>
-            <p className="book-card__subtitle">Collecting More Data from the Modern Web</p>
+            <h4 className="book-card__title">{title}</h4>
+            <p className="book-card__subtitle">{subtitle}</p>
             <div className="book-card__controls-row">
-                <span className="book-card__price">$33.99</span>
+                <span className="book-card__price">{price}</span>
             </div>
         </div>
     )
--- src/components/books-page/BooksPage.tsx
+++ src/components/books-page/BooksPage.tsx
@@ -14,10 +14,30 @@ const BooksPage: React.FC<PropsType> = () => {
                     New Books
                 </h2>
                 <div className="books-wrap">
-                    <BookCard />
-                    <BookCard />
-                    <BookCard />
-                    <BookCard />
+                    <BookCard
+                        title="Designing Across Senses"
+                        subtitle="A Multimodal Approach to Product Desig"
+                        price="$27.59"
+                        img="https://itbook.store/img/books/9781491954249.png"
+                    />
+                    <BookCard
+                        title="Web Scraping with Python, 2nd Edition"
+                        subtitle="Collecting More Data from the Modern Web"
+                        price="$33.9"
+                        img="https://itbook.store/img/books/9781491985571.png"
+                    />
+                    <BookCard
+                        title="Programming iOS 11"
+                        subtitle="Dive Deep into Views, View Controllers, and Frameworks"
+                        price="$59.17"
+                        img="https://itbook.store/img/books/9781491999226.png"
+                    />
+                    <BookCard
+                        title="Microsoft Excel Inside Out"
+                        subtitle="Office 2021 and Microsoft 365"
+                        price="$42.16"
+                        img="https://itbook.store/img/books/9780137559534.png"
+                    />
                 </div>
             </div>
         </section>
