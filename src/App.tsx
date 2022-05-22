8ffca822cdf2aa34d9f5698b575d8c0d7cd1d424
Natalya <natalya.karaychentseva@gmail.com>
2022-05-22 17:53:05 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import BookDetailsPage from './components/book-details-page/BookDetailsPage';
 import BooksPage from './components/books-page/BooksPage';
 import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* <BooksPage /> */}
-      <BookDetailsPage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/books" >
+            <Route index element={<BooksPage/>} />
+            <Route path=":id" element={<BookDetailsPage/>} />
+          </Route>
+          <Route path="*" element={<Navigate to={"/books"}/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
