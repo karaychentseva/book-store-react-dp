0cd130421d43e9b6d4f8fb4353c2e8b39e261be2
Natalya <natalya.karaychentseva@gmail.com>
2022-05-28 21:42:04 +0300

    add navigation to home page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
 import { ReactComponent as Logo } from "../../assets/book-open-reader-solid.svg";
 import "./Header.scss";
@@ -8,12 +9,16 @@ type PropsType = {
 }
 
 const Header: React.FC<PropsType> = () => {
+    const navigate = useNavigate();
+    const handleHeaderClick = () => {
+        navigate(`/books/`);
+    }
 
     return (
         <header className="header">
             <div className="container">
                 <div className="header-wrap">
-                    <div className="header-title">
+                    <div className="header-title" onClick={handleHeaderClick}>
                         <div className="header-title__logo">
                             <Logo className='header-title__logo-item'/>
                         </div>
