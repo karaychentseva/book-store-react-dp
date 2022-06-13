acf8b7eb7a856b9a76fb61556b158ac69d59bc2b
Natalya <natalya.karaychentseva@gmail.com>
2022-06-13 09:59:27 +0300

    fix theme text

--- src/components/header/theme-toggle/ThemeToggle.tsx
+++ src/components/header/theme-toggle/ThemeToggle.tsx
@@ -2,6 +2,7 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 import * as React from 'react';
 import { useActions } from '../../../hooks/useActions';
 import { useSelector } from '../../../hooks/useSelector';
+import { Themes } from '../../../enums/themes';
 import './ThemeToggle.scss'
 
 type PropsType = {
@@ -12,10 +13,10 @@ const ThemeToggle: React.FC<PropsType> = () => {
     const theme = useSelector(state => state.theme.theme);
 
     const changeTheme = () => {
-        if (theme === 'white') {
-            setTheme('dark');
+        if (theme === Themes.white) {
+            setTheme(Themes.dark);
         } else {
-            setTheme('white');
+            setTheme(Themes.white);
         }
     }
     return (
